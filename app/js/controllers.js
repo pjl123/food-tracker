2a7e927ce97dd2e4d3eb4e5a40e9e54d353955cf
Patrick LaFata <lafatapatrick@gmail.com>
2014-11-28 18:15:44 -0500

    Added mock food data and css structure

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -23,10 +23,22 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Ph
 
 phonecatControllers.controller('FoodsCtrl', ['$scope','$localStorage', function ($scope,$localStorage){
   $scope.$storage = $localStorage.$default({
-    foods:[]
+    foods:[
+      {name:"steak",type:"protein",quantity:{numeric:1,units:"pound"}},
+      {name:"apple",type:"fruit",quantity:{numeric:4,units:"item"}},
+      {name:"spinach",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
+      {name:"potato chips",type:"snack",quantity:{numeric:1,units:"item"}},
+      {name:"celery",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
+      {name:"rice",type:"grain",quantity:{numeric:1,units:"pound"}},
+      {name:"black beans",type:"legume",quantity:{numeric:8,units:"ounce"}},
+      {name:"yogurt",type:"dairy",quantity:{numeric:6,units:"ounce"}},
+      {name:"orange",type:"fruit",quantity:{numeric:3,units:"item"}},
+      {name:"bread",type:"grain",quantity:{numeric:1,units:"item"}}
+    ]
   });
   $scope.foods = $scope.$storage.foods;
   $scope.food = {};
+
   $scope.query = "";
   $scope.category = "";
 
@@ -39,6 +51,23 @@ phonecatControllers.controller('FoodsCtrl', ['$scope','$localStorage', function 
   $scope.removeFood = function(food){
     var i = $scope.foods.indexOf(food);
     $scope.foods.splice(i,1);
+  };
+
+  $scope.resetData = function(){
+    $localStorage.$reset({
+      foods:[
+        {name:"steak",type:"protein",quantity:{numeric:1,units:"pound"}},
+        {name:"apple",type:"fruit",quantity:{numeric:4,units:"item"}},
+        {name:"spinach",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
+        {name:"potato chips",type:"snack",quantity:{numeric:1,units:"item"}},
+        {name:"celery",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
+        {name:"rice",type:"grain",quantity:{numeric:1,units:"pound"}},
+        {name:"black beans",type:"legume",quantity:{numeric:8,units:"ounce"}},
+        {name:"yogurt",type:"dairy",quantity:{numeric:6,units:"ounce"}},
+        {name:"orange",type:"fruit",quantity:{numeric:3,units:"item"}},
+        {name:"bread",type:"grain",quantity:{numeric:1,units:"item"}}
+      ]
+    });
   };
 
 }]);
