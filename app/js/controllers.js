2534e41e8a254225dea43a5486acf22ea0a009f8
Patrick LaFata <lafatapatrick@gmail.com>
2014-11-30 11:27:44 -0500

    Added editing to foods list

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,20 +25,20 @@ foodTrackerControllers.controller('MainCtrl', function ($scope){
   
 });
 
-foodTrackerControllers.controller('FoodsCtrl', ['$scope','$localStorage','$route', function ($scope,$localStorage,$route){
+foodTrackerControllers.controller('FoodsCtrl', ['$scope','$localStorage','$route','$log', function ($scope,$localStorage,$route,$log){
   $scope.$storage = $localStorage.$default({
     recipes:[],
     foods:[
-      {name:"steak",type:"protein",quantity:{numeric:1,units:"pound"}},
-      {name:"apple",type:"fruit",quantity:{numeric:4,units:"item"}},
-      {name:"spinach",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
-      {name:"potato chips",type:"snack",quantity:{numeric:1,units:"item"}},
-      {name:"celery",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
-      {name:"rice",type:"grain",quantity:{numeric:1,units:"pound"}},
-      {name:"black beans",type:"legume",quantity:{numeric:8,units:"ounce"}},
-      {name:"yogurt",type:"dairy",quantity:{numeric:6,units:"ounce"}},
-      {name:"orange",type:"fruit",quantity:{numeric:3,units:"item"}},
-      {name:"bread",type:"grain",quantity:{numeric:1,units:"item"}}
+      {name:"steak",type:"protein",quantity:{numeric:1,units:"pound"},showEdit:false},
+      {name:"apple",type:"fruit",quantity:{numeric:4,units:"item"},showEdit:false},
+      {name:"spinach",type:"vegetable",quantity:{numeric:0.5,units:"pound"},showEdit:false},
+      {name:"potato chips",type:"snack",quantity:{numeric:1,units:"item"},showEdit:false},
+      {name:"celery",type:"vegetable",quantity:{numeric:0.5,units:"pound"},showEdit:false},
+      {name:"rice",type:"grain",quantity:{numeric:1,units:"pound"},showEdit:false},
+      {name:"black beans",type:"legume",quantity:{numeric:8,units:"ounce"},showEdit:false},
+      {name:"yogurt",type:"dairy",quantity:{numeric:6,units:"ounce"},showEdit:false},
+      {name:"orange",type:"fruit",quantity:{numeric:3,units:"item"},showEdit:false},
+      {name:"bread",type:"grain",quantity:{numeric:1,units:"item"},showEdit:false}
     ]
   });
   $scope.foods = $scope.$storage.foods;
@@ -50,6 +50,7 @@ foodTrackerControllers.controller('FoodsCtrl', ['$scope','$localStorage','$route
 
   $scope.addFood = function(){
     var newFood = $scope.food;
+    newFood.showEdit=false;
     $scope.foods.push(newFood);
     $scope.food = {};
   };
@@ -63,16 +64,16 @@ foodTrackerControllers.controller('FoodsCtrl', ['$scope','$localStorage','$route
     $localStorage.$reset({
       recipes:$scope.$storage.recipes,
       foods:[
-        {name:"steak",type:"protein",quantity:{numeric:1,units:"pound"}},
-        {name:"apple",type:"fruit",quantity:{numeric:4,units:"item"}},
-        {name:"spinach",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
-        {name:"potato chips",type:"snack",quantity:{numeric:1,units:"item"}},
-        {name:"celery",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
-        {name:"rice",type:"grain",quantity:{numeric:1,units:"pound"}},
-        {name:"black beans",type:"legume",quantity:{numeric:8,units:"ounce"}},
-        {name:"yogurt",type:"dairy",quantity:{numeric:6,units:"ounce"}},
-        {name:"orange",type:"fruit",quantity:{numeric:3,units:"item"}},
-        {name:"bread",type:"grain",quantity:{numeric:1,units:"item"}}
+        {name:"steak",type:"protein",quantity:{numeric:1,units:"pound"},showEdit:false},
+        {name:"apple",type:"fruit",quantity:{numeric:4,units:"item"},showEdit:false},
+        {name:"spinach",type:"vegetable",quantity:{numeric:0.5,units:"pound"},showEdit:false},
+        {name:"potato chips",type:"snack",quantity:{numeric:1,units:"item"},showEdit:false},
+        {name:"celery",type:"vegetable",quantity:{numeric:0.5,units:"pound"},showEdit:false},
+        {name:"rice",type:"grain",quantity:{numeric:1,units:"pound"},showEdit:false},
+        {name:"black beans",type:"legume",quantity:{numeric:8,units:"ounce"},showEdit:false},
+        {name:"yogurt",type:"dairy",quantity:{numeric:6,units:"ounce"},showEdit:false},
+        {name:"orange",type:"fruit",quantity:{numeric:3,units:"item"},showEdit:false},
+        {name:"bread",type:"grain",quantity:{numeric:1,units:"item"},showEdit:false}
       ]
     });
     $route.reload();
@@ -84,16 +85,16 @@ foodTrackerControllers.controller('RecipeCtrl', ['$scope','$localStorage','$http
   $scope.$storage = $localStorage.$default({
     recipes:[],
     foods:[
-      {name:"steak",type:"protein",quantity:{numeric:1,units:"pound"}},
-      {name:"apple",type:"fruit",quantity:{numeric:4,units:"item"}},
-      {name:"spinach",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
-      {name:"potato chips",type:"snack",quantity:{numeric:1,units:"item"}},
-      {name:"celery",type:"vegetable",quantity:{numeric:0.5,units:"pound"}},
-      {name:"rice",type:"grain",quantity:{numeric:1,units:"pound"}},
-      {name:"black beans",type:"legume",quantity:{numeric:8,units:"ounce"}},
-      {name:"yogurt",type:"dairy",quantity:{numeric:6,units:"ounce"}},
-      {name:"orange",type:"fruit",quantity:{numeric:3,units:"item"}},
-      {name:"bread",type:"grain",quantity:{numeric:1,units:"item"}}
+      {name:"steak",type:"protein",quantity:{numeric:1,units:"pound"},showEdit:false},
+      {name:"apple",type:"fruit",quantity:{numeric:4,units:"item"},showEdit:false},
+      {name:"spinach",type:"vegetable",quantity:{numeric:0.5,units:"pound"},showEdit:false},
+      {name:"potato chips",type:"snack",quantity:{numeric:1,units:"item"},showEdit:false},
+      {name:"celery",type:"vegetable",quantity:{numeric:0.5,units:"pound"},showEdit:false},
+      {name:"rice",type:"grain",quantity:{numeric:1,units:"pound"},showEdit:false},
+      {name:"black beans",type:"legume",quantity:{numeric:8,units:"ounce"},showEdit:false},
+      {name:"yogurt",type:"dairy",quantity:{numeric:6,units:"ounce"},showEdit:false},
+      {name:"orange",type:"fruit",quantity:{numeric:3,units:"item"},showEdit:false},
+      {name:"bread",type:"grain",quantity:{numeric:1,units:"item"},showEdit:false}
     ]
   });
   $scope.recipes = $scope.$storage.recipes;
@@ -107,8 +108,8 @@ foodTrackerControllers.controller('RecipeCtrl', ['$scope','$localStorage','$http
   var getRandomIngredients = function() {
     // copy food array
     var foods = $scope.$storage.foods.slice(0);
-    // get random of ingredients between 1 and number of foods in pantry
-    var num_ingredients = Math.ceil(Math.random()*foods.length);
+    // get 3 ingredients from the foods in pantry
+    var num_ingredients = 3;
     var ingredients = '';
     for (var i = 0; i < num_ingredients && num_ingredients <= foods.length; i++) {
       // get random food and remove it from temp food array
@@ -131,8 +132,10 @@ foodTrackerControllers.controller('RecipeCtrl', ['$scope','$localStorage','$http
 
     var params = {
       api_key: apiKey,
-      responseType: 'json'
-      //q: ingredients
+      responseType: 'json',
+      rpp: 1,
+      pg: Math.ceil(Math.random()*50),
+      any_kw: ingredients
     };
     var config = {
       params: params
